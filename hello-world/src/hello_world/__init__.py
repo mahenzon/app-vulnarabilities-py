59bcf47319c156d952b3b985bc1d0a42b747dbc8
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-02-22 13:48:06 +0300

    configure pre-commit

--- hello-world/src/hello_world/__init__.py
+++ hello-world/src/hello_world/__init__.py
@@ -4,5 +4,6 @@ This repo contains multiple projects to demonstrate vulnarabilities in Python we
 This hello-world package is just a placeholder to init lock file properly.
 """
 
+
 def main() -> None:
     print(text)
