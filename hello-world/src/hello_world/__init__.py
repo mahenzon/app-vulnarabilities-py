a5200e1836495d96ddefc5aa9c5f38115d534c17
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-02-22 13:41:03 +0300

    add hello-world package

---
+++ hello-world/src/hello_world/__init__.py
@@ -0,0 +1,8 @@
+text = """\
+This repo contains multiple projects to demonstrate vulnarabilities in Python web apps.
+
+This hello-world package is just a placeholder to init lock file properly.
+"""
+
+def main() -> None:
+    print(text)
